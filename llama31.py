ee12d6c89bab0c184663ffdceba0151f015488fc
hadiz3m <mj.hadi@gmail.com>
2024-08-19 15:07:50 +0330

    ok

--- llama31.py
+++ llama31.py
@@ -659,9 +659,9 @@ def main(
     tokenizer_path: str = "tokenizer.model",
     temperature: float = 1.0,
     top_p: float = 0.9,
-    max_seq_len: int = 64,
-    max_gen_len: int = 256,
-    max_batch_size: int = 256,
+    max_seq_len: int = 32,
+    max_gen_len: int = 128,
+    max_batch_size: int = 128,
     flash: bool = True,
 ):
 
